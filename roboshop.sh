3e21a05b9daa2b3c968dae6f7fa598d1646a15c7
Suresh Pachari <pacharipallisuresh@gmail.com>
2026-02-13 00:03:20 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -4,7 +4,9 @@ SG_ID="sg-0ef724a4dcf78d048"
 AMI_ID="ami-0220d79f3f480ecf5"
 
 
-for INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID \
+for instance in $@
+do
+     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID \
      --instance-type t3.micro \
      --security-group-ids $SG_ID \
      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
